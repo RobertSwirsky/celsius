ffede85b3bf5763a5c241546e68d4a82c96e3a1b
Robert Swirsky <swirsky@gmail.com>
2025-05-06 15:53:18 -0700

    Basic web server

--- src/celsius/celsius.py
+++ src/celsius/celsius.py
@@ -1,6 +1,7 @@
 from w1thermsensor import W1ThermSensor, Unit
 import RPi.GPIO as GPIO
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import celsius_server
 import time
 
 def initialize_soft_pullups(pullups):
@@ -9,7 +10,7 @@ def initialize_soft_pullups(pullups):
     
 def print_temperatures():
     for sensor in W1ThermSensor.get_available_sensors():
-        print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature())
+        print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
 
 if __name__== "__main__":
     print("main")
@@ -17,3 +18,16 @@ if __name__== "__main__":
     sensor_list = [4]
     initialize_soft_pullups(sensor_list)
     print_temperatures()
+
+
+    webServer = HTTPServer(("localhost", 8080), celsius_server.CelsiusServer)
+    print("Server started http://%s:%s" % ("localhost", 8080))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
+
