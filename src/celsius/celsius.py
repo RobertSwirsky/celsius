20099cc7ef4dd5f2a3d05251640519d026f400e3
Robert Swirsky <swirsky@gmail.com>
2025-05-07 16:00:19 -0700

    Simple webserver based on flask to serve temp sesnor readings in HTML and json Z

--- src/celsius/celsius.py
+++ src/celsius/celsius.py
@@ -1,33 +1,24 @@
 from w1thermsensor import W1ThermSensor, Unit
 import RPi.GPIO as GPIO
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import celsius_server
 import time
 
-def initialize_soft_pullups(pullups):
-    for p in pullups:
-        GPIO.setup(p, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    
-def print_temperatures():
-    for sensor in W1ThermSensor.get_available_sensors():
-        print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
+class sensor:
+    def __init__(self, sensor_list):
+        GPIO.setmode(GPIO.BCM)
+        self.sensor_list = sensor_list
+        self.initialize_soft_pullups()
 
-if __name__== "__main__":
-    print("main")
-    GPIO.setmode(GPIO.BCM)
-    sensor_list = [4]
-    initialize_soft_pullups(sensor_list)
-    print_temperatures()
+    def initialize_soft_pullups(self):
+        for p in self.sensor_list:
+            GPIO.setup(p, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        
+    def print_temperatures(self):
+        for sensor in W1ThermSensor.get_available_sensors():
+            print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
+
+    def get_sensor_list(self):
+        return W1ThermSensor.get_available_sensors()
 
 
-    webServer = HTTPServer(("localhost", 8080), celsius_server.CelsiusServer)
-    print("Server started http://%s:%s" % ("localhost", 8080))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
 
--- src/celsius/celsius_server.py
+++ src/celsius/celsius_server.py
@@ -1,13 +1,21 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
+from flask import Flask, jsonify, request, render_template_string, current_app
+from celsius import sensor
 
-class CelsiusServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+app = Flask(__name__)
+@app.route('/')
+def data():
+    # Check if client wants JSON or HTML (default to JSON)
+    data_list = (current_app.config['celsius']).get_sensor_list()
+    if request.args.get('format') == 'html':
+        # Render list as HTML unordered list
+        html_content = '<ul>' + ''.join(f'<li>{item.id},&nbsp;{item.get_temperature()}</li>' for item in data_list) + '</ul>'
+        return render_template_string(html_content)
+    else:
+        # Return JSON response
+        return jsonify([ (d.id, d.get_temperature()) for d in data_list ])
+
+
+if __name__ == '__main__':
+    app.config['celsius'] = sensor([4])
+    app.run(host='0.0.0.0',debug=True)
+
