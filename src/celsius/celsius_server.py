8de3ccf3aca15fb6e630e1a7ee7d17ca4dc052b4
Robert Swirsky <swirsky@gmail.com>
2025-05-14 15:32:26 -0700

    Add UTC time to temperature readings Z# Please enter the commit message for your changes. Lines starting

--- src/celsius/celsius_server.py
+++ src/celsius/celsius_server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, render_template_string, current_app
 from celsius import sensor
+from  datetime import datetime, timezone
 
 
 app = Flask(__name__)
@@ -12,12 +13,14 @@ def data():
 
     if request.args.get('format') == 'html':
         # Render list as HTML unordered list
-        html_content = '<ul>' + ''.join(f'<li>{item.id},&nbsp;{item.get_temperature()}</li>' for item in data_list) + '</ul>'
+        now_utc_str = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+        html_content = '<ul>' + ''.join(f'<li>{item.id},&nbsp;{item.get_temperature()},&nbsp;{now_utc_str}</li>' for item in data_list) + '</ul>'
         return render_template_string(html_content)
     else:
         # Return JSON response
         try:
-            return jsonify([ (d.id, d.get_temperature()) for d in data_list ])
+            now_utc_str = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+            return jsonify([ (now_utc_str, d.id, d.get_temperature()) for d in data_list ])
         except w1thermsensor.SensorNotReadyError:
             return jsonify([('error', 'SensorNotReady')])
 
