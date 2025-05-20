457b361fa1765be9f3570ca626edf248740c792a
Robert Swirsky <swirsky@gmail.com>
2025-05-20 16:05:39 -0700

    Keep last 100 readings

--- src/celsius/celsius_server.py
+++ src/celsius/celsius_server.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, request, render_template_string, current_app
 from celsius import sensor
 from  datetime import datetime, timezone
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
+from collections import deque
+
 
 
 app = Flask(__name__)
@@ -9,26 +13,53 @@ def data():
     import w1thermsensor
     # Check if client wants JSON or HTML (default to JSON)
     data_list = (current_app.config['celsius']).get_sensor_list()
-    # XXX TODO: Put in try block to catch "SensorNotReady" error
+    
+    # get deques for all the sensors
+    sensor_dict = (current_app.config['logTemps']).sensor_dict
+
 
     if request.args.get('format') == 'html':
         # Render list as HTML unordered list
-        now_utc_str = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
-        html_content = '<ul>' + ''.join(f'<li>{item.id},&nbsp;{item.get_temperature()},&nbsp;{now_utc_str}</li>' for item in data_list) + '</ul>'
+        html_content = "<ul>"
+        for k in sensor_dict:
+            newest = (sensor_dict[k])[-1]
+            html_content = html_content + "<li>" + k + ", " +  newest[0] + ", " + newest[1] + "</li>"
+        html_content = html_content + "</ul>"
         return render_template_string(html_content)
     else:
         # Return JSON response
-        try:
-            now_utc_str = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
-            return jsonify([ (now_utc_str, d.id, d.get_temperature()) for d in data_list ])
-        except w1thermsensor.SensorNotReadyError:
-            return jsonify([('error', 'SensorNotReady')])
+        result = []
+        for k in sensor_dict:
+            newest = (sensor_dict[k])[-1]
+            result.append((k, newest[0], newest[1]))
+        return jsonify(result)
 
+        
+class logTemperatures:
+    def __init__(self, sensor):
+        self.sensor = sensor
+        self.sensor_dict = {}           # empty dict to hold last "n" readings
+    
+    def job(self):
+        data = self.sensor.get_sensor_list()
+        now_utc_str = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+        for d in data:
+            if not d.id in self.sensor_dict:
+                self.sensor_dict[d.id] = deque(maxlen=100)
+            self.sensor_dict[d.id].append( (now_utc_str, d.get_temperature() ))
 
 if __name__ == '__main__':
     my_sensor = sensor([4])
     if my_sensor.check_kernel_modules():
+        logTemps = logTemperatures(my_sensor)
+        logTemps.job()
         app.config['celsius'] = my_sensor
+        app.config['logTemps'] = logTemps
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(func=logTemps.job, trigger='interval', minutes=1)
+        scheduler.start()
+        atexit.register(lambda: scheduler.shutdown())
         app.run(host='0.0.0.0',debug=True)
 
 
